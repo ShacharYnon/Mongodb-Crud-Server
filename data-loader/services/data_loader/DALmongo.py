6fc91319ec06296e03fbb1c2772dddfde9fbd4eb
Shachar <shar535909@gmail.com>
2025-08-19 17:52:36 +0300

    Remove README.md and add initial Dockerfile and DAL files

---
+++ data-loader/services/data_loader/DALmongo.py
@@ -0,0 +1,24 @@
+import pymongo
+import uuid
+
+class DALmongo:
+
+    def __init__(self ,first_name:str ,last_name:str ,phone_number:str ,rank:str):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = phone_number
+        self.rank = rank
+        self.ID = uuid.uuid4()
+
+
+    def find_collection(self):
+        pass
+
+    def insert_to_collection(self):
+        pass
+
+    def update_to_collection(self):
+        pass
+
+    def delete_to_collection(self):
+        pass
